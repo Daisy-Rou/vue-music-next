7c1ec6f22a5f0de16fdf827959f4fcbc3d543c82
huangrou <daixi@yscredit.com>
2021-04-19 17:32:42 +0800

    feat: 1、better-scroll滚动插件

---
+++ src/components/base/scroll/use-scroll.js
@@ -0,0 +1,17 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+import BScroll from '@better-scroll/core'
+import ObserveDOM from '@better-scroll/observe-dom'
+BScroll.use(ObserveDOM)
+
+export default function useScroll(wrapperRef, options) {
+  const scroll = ref(null)
+  onMounted(() => {
+    scroll.value = new BScroll(wrapperRef.value, {
+      observeDOM: true,
+      ...options
+    })
+  })
+  onUnmounted(() => {
+    scroll.value.destroy()
+  })
+}
