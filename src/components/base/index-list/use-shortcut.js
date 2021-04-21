4b743273c1930493ceae0e18715134743beb9716
huangrou <daixi@yscredit.com>
2021-04-21 09:58:36 +0800

    feat: 1、右侧快捷导航拖动点击到对应列表区域

--- src/components/base/index-list/use-shortcut.js
+++ src/components/base/index-list/use-shortcut.js
@@ -1,7 +1,10 @@
 import { computed, ref } from 'vue'
 
 export default function useShortcut(props, groupRef) {
+  // 锚点高度
+  const ANCHOR_HEIGHT = 18
   const scrollRef = ref(null)
+  const touch = {}
 
   const shortcutList = computed(() => {
     return props.data.map((group) => {
@@ -13,17 +16,37 @@ export default function useShortcut(props, groupRef) {
   function onShortcutTouchStart(e) {
     // 通过绑定data-index属性获取当前滚动index
     const anchorIndex = parseInt(e.target.dataset.index)
-    const targetEl = groupRef.value.children[anchorIndex]
+    // 第一个纵坐标
+    touch.y1 = e.touches[0].pageY
+    // 将索引保留到touch对象里
+    touch.anchorIndex = anchorIndex
+    scrollTo(anchorIndex)
+  }
+
+  function onShortcutTouchMove(e) {
+    // 第二个纵坐标
+    touch.y2 = e.touches[0].pageY
+    // 手指滑动距离 /锚点高度 = 偏移索引
+    const delta = (touch.y2 - touch.y1) / ANCHOR_HEIGHT | 0
+    // 目标索引
+    const anchorIndex = touch.anchorIndex + delta
+    scrollTo(anchorIndex)
+  }
+
+  function onShortcutTouchEnd(e) {
+
+  }
+
+  // 函数封装
+  function scrollTo(index) {
+    // 在右侧黑色区域拖动报错处理
+    if (isNaN(index)) {
+      return
+    }
+    index = Math.max(0, Math.min(shortcutList.value.length - 1, index))
+    const targetEl = groupRef.value.children[index]
     const scroll = scrollRef.value.scroll
-    scroll.scrollToElement(targetEl)
-  }
-
-  function onShortcutTouchMove() {
-
-  }
-
-  function onShortcutTouchEnd() {
-
+    scroll.scrollToElement(targetEl, 0)
   }
   return {
     shortcutList,
