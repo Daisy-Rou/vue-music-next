644550847de55bd6b9ddbbcaf6868baf3abb4e98
huangrou <daixi@yscredit.com>
2021-04-22 16:28:04 +0800

    feat: 播放器播放按钮播放与暂停状态逻辑

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -12,10 +12,30 @@
           <h1 class="title">{{currentSong.name}}</h1>
           <h2 class="subtitle">{{currentSong.singer}}</h2>
         </div>
+        <div class="bottom">
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="icon-sequence"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="icon-prev"></i>
+            </div>
+            <div class="icon i-center">
+              <i :class="playIcon" @click="togglePlay"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-next"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="icon-favorite"></i>
+            </div>
+          </div>
+        </div>
       </template>
     </div>
     <audio
       ref="audioRef"
+      @pause="pause"
     >
     </audio>
   </div>
@@ -34,7 +54,14 @@ export default {
     const store = useStore()
     const fullScreen = computed(() => store.state.fullScreen)
     const currentSong = computed(() => store.getters.currentSong)
+    // 播放状态
+    const playing = computed(() => store.state.playing)
+    // 根据播放状态展示不同图标
+    const playIcon = computed(() => {
+      return playing.value ? 'icon-pause' : 'icon-play'
+    })
 
+    // 当前播放歌曲
     watch(currentSong, (newSong) => {
       if (!newSong.id || !newSong.url) {
         return
@@ -44,15 +71,33 @@ export default {
       audioEl.play()
     })
 
+    // 监听歌曲播放状态
+    watch(playing, (newPlaying) => {
+      const audioEl = audioRef.value
+      newPlaying ? audioEl.play() : audioEl.pause()
+    })
+
     function goBack() {
       store.commit('setFullScreen', false)
+    }
+
+    function togglePlay() {
+      store.commit('setPlayingState', !playing.value)
+    }
+
+    // 电脑待机或电脑屏幕关闭 关闭音乐
+    function pause() {
+      store.commit('setPlayingState', false)
     }
 
     return {
       audioRef,
       fullScreen,
       currentSong,
-      goBack
+      goBack,
+      playIcon,
+      togglePlay,
+      pause
     }
   }
 }
@@ -113,6 +158,97 @@ export default {
         text-align: center;
         font-size: $font-size-medium;
         color: $color-text;
+      }
+    }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .dot-wrapper {
+        text-align: center;
+        font-size: 0;
+        .dot {
+          display: inline-block;
+          vertical-align: middle;
+          margin: 0 4px;
+          width: 8px;
+          height: 8px;
+          border-radius: 50%;
+          background: $color-text-l;
+          &.active {
+            width: 20px;
+            border-radius: 5px;
+            background: $color-text-ll;
+          }
+        }
+      }
+      .progress-wrapper {
+        display: flex;
+        align-items: center;
+        width: 80%;
+        margin: 0px auto;
+        padding: 10px 0;
+        .time {
+          color: $color-text;
+          font-size: $font-size-small;
+          flex: 0 0 40px;
+          line-height: 30px;
+          width: 40px;
+          &.time-l {
+            text-align: left;
+          }
+          &.time-r {
+            text-align: right;
+          }
+        }
+        .progress-bar-wrapper {
+          flex: 1;
+        }
+      }
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .i-left {
+          text-align: right;
+        }
+        .i-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .i-right {
+          text-align: left
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
+      }
+    }
+    &.normal-enter-active, &.normal-leave-active {
+      transition: all .6s;
+      .top, .bottom {
+        transition: all .6s cubic-bezier(0.45, 0, 0.55, 1);
+      }
+    }
+    &.normal-enter-from, &.normal-leave-to {
+      opacity: 0;
+      .top {
+        transform: translate3d(0, -100px, 0);
+      }
+      .bottom {
+        transform: translate3d(0, 100px, 0)
       }
     }
   }
